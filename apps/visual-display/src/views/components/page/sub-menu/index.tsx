e036eea0e501304604247b719ab9c7a4c16d3ea8
史连宁 <shilianning@yuanxinjituan.com>
2025-12-09 17:13:05 +0800

    chore: chore: eslint集成
    no

--- apps/visual-display/src/views/components/page/sub-menu/index.tsx
+++ apps/visual-display/src/views/components/page/sub-menu/index.tsx
@@ -1,12 +1,12 @@
 import { type DefineComponent, defineComponent } from 'vue';
 
 const SubMenu: DefineComponent = defineComponent({
-    name: 'SubMenu',
-    setup() {
-        const handleOpen = () => {};
-        const handleClose = () => {};
-        return () => <el-menu default-active="2" class="el-menu-vertical-demo" onOpen={handleOpen} onClose={handleClose}></el-menu>;
-    },
+  name: 'SubMenu',
+  setup() {
+    const handleOpen = () => {};
+    const handleClose = () => {};
+    return () => <el-menu default-active="2" class="el-menu-vertical-demo" onOpen={handleOpen} onClose={handleClose}></el-menu>;
+  },
 });
 
 export default SubMenu;
