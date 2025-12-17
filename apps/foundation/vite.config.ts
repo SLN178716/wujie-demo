9e1e3fee9eb3f3697d6c978ea86f619832c0719b
史连宁 <shilianning@yuanxinjituan.com>
2025-12-17 10:12:27 +0800

    chore: 打包优化

--- apps/foundation/vite.config.ts
+++ apps/foundation/vite.config.ts
@@ -3,11 +3,13 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 // @ts-expect-error: 暂无解决
 import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
+    base: `/${__dirname.split(path.sep).pop()}/`,
     plugins: [
       react(),
       eslintPlugin(),
